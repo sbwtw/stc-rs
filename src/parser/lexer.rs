727b7e528516de849c6bbdbfed4ad3435c2efa07
石博文 <sbw@sbw.so>
2021-04-23 20:10:18 +0800

    add string parse

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -3,6 +3,8 @@ use std::str::CharIndices;
 use crate::ast::{AstNode, AstVisitor};
 
 pub type Spanned<Tok, Loc, Error> = Result<(Loc, Tok, Loc), Error>;
+pub type LexerResult = Spanned<Tok, usize, LexicalError>;
+
 #[derive(Debug, Clone)]
 pub enum Tok {
     Plus,
@@ -33,6 +35,7 @@ impl AstNode for LiteralType {
 #[derive(Debug)]
 pub enum LexicalError {
     UnexpectedCharacter(usize, char),
+    UnexpectedEnd,
 }
 
 pub struct Lexer<'input> {
@@ -50,7 +53,7 @@ impl<'input> Lexer<'input> {
         &mut self,
         start: usize,
         ch: char,
-    ) -> Option<Spanned<Tok, usize, LexicalError>> {
+    ) -> Option<LexerResult> {
         let mut s = String::from(ch);
         let mut end = start;
 
@@ -77,7 +80,7 @@ impl<'input> Lexer<'input> {
         &mut self,
         start: usize,
         mut s: String,
-    ) -> Option<Spanned<Tok, usize, LexicalError>> {
+    ) -> Option<LexerResult> {
         let mut dot = false;
         let mut end = start;
 
@@ -105,10 +108,32 @@ impl<'input> Lexer<'input> {
             }
         }
     }
+
+    fn parse_string(&mut self, start: usize) -> Option<LexerResult> {
+        let mut escape = false;
+        let mut s = String::new();
+
+        loop {
+            if escape == true {
+                escape = false;
+
+                match self.chars.next()? {
+                    (_, '\"') => s.push('\"'),
+                    (n, c) => return Some(Err(LexicalError::UnexpectedCharacter(n, c))),
+                }
+            }
+
+            match self.chars.next()? {
+                (_, '\\') => escape = true,
+                (n, '\"') => return Some(Ok((start, Tok::Literal(LiteralType::String(s)), n + 1))),
+                (_, c) => s.push(c),
+            }
+        }
+    }
 }
 
 impl<'input> Iterator for Lexer<'input> {
-    type Item = Spanned<Tok, usize, LexicalError>;
+    type Item = LexerResult;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.chars.next()? {
@@ -121,6 +146,7 @@ impl<'input> Iterator for Lexer<'input> {
             (i, ')') => Some(Ok((i, Tok::RightParentheses, i + 1))),
             (i, ',') => Some(Ok((i, Tok::Comma, i + 1))),
             (i, ';') => Some(Ok((i, Tok::Semicolon, i + 1))),
+            (i, '\"') => self.parse_string(i),
             (start, c) if c.is_ascii_digit() && c != '0' => self.parse_integer(start, c),
             (i, c) => Some(Err(LexicalError::UnexpectedCharacter(i, c))),
         }
